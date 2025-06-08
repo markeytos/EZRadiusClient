5130b0e0897f0209787566225b3df371f956db3a
Igal <igalfsg@gmail.com>
2025-06-08 16:45:23 -0400

    tried locally

--- SampleApp/buildAndSign.ps1
+++ SampleApp/buildAndSign.ps1
@@ -2,8 +2,10 @@ param (
     [string] $signingCertName = "globalsign",
     [string] $signingAKV = "https://codesigningkeytos.vault.azure.net/"
 )
+# $env:Path += ";C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin" 
+# $env:Path += ";C:\Program Files (x86)\Windows Kits\10\App Certification Kit"
 msbuild .\SampleApp\SampleApp.csproj /restore /t:publish  /p:Configuration=Release /p:PublishSingleFile=True /p:SelfContained=True /p:Platform=x64 /p:RuntimeIdentifier=win-x64
 pwd
 ls
 $akvToken = (az account get-access-token  --resource https://vault.azure.net --query "accessToken").Replace('"','')
-azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\EZRadiusClient\SampleApp\bin\Release\net8.0-windows\win-x64\publish\*.exe
+azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\SampleApp\bin\x64\Release\net8.0\win-x64\publish\*.exe
