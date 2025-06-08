b5462579e93ebcd21484fd51f3e8d584bee7ecec
Igal <igalfsg@gmail.com>
2025-06-08 16:41:47 -0400

    troubleshooting NuGet

--- EZRadiusClient/CreateNuget.ps1
+++ EZRadiusClient/CreateNuget.ps1
@@ -5,5 +5,5 @@ param (
 )
 msbuild .\SampleApp\SampleApp.csproj /restore /t:publish  /p:Configuration=Release /p:PublishSingleFile=True /p:SelfContained=True /p:Platform=x64 /p:RuntimeIdentifier=win-x64
 $akvToken = (az account get-access-token  --resource https://vault.azure.net --query "accessToken").Replace('"','')
-azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\EZRadiusClient\bin\Release\EZRadiusClient.$($nugetVersion).nupkg
-dotnet nuget verify --all .\EZRadiusClient\bin\Release\EZRadiusClient.$($nugetVersion).nupkg
+azuresigntool sign --azure-key-vault-url $signingAKV -kvc $signingCertName --azure-key-vault-accesstoken $akvToken -tr http://timestamp.digicert.com .\EZRadiusClient\EZRadiusClient\bin\Release\EZRadiusClient.$($nugetVersion).nupkg
+dotnet nuget verify --all .\EZRadiusClient\EZRadiusClient\bin\Release\EZRadiusClient.$($nugetVersion).nupkg
