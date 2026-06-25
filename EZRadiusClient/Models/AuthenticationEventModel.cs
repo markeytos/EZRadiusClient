be3ec833a72ebd0818bc37545b68f4fdb50df77a
igal <igalfsg@gmail.com>
2026-06-25 18:27:00 -0400

    fix: added missing fields

--- EZRadiusClient/Models/AuthenticationEventModel.cs
+++ EZRadiusClient/Models/AuthenticationEventModel.cs
@@ -5,10 +5,10 @@ namespace EZRadiusClient.Models;
 public class AuthenticationEventModel
 {
     [JsonPropertyName("SubscriptionId")]
-    public string? SubscriptionId { get; set; }
+    public string SubscriptionId { get; set; } = string.Empty;
 
     [JsonPropertyName("RadiusSubscriptionTenantId")]
-    public string? RadiusSubscriptionTenantId { get; set; }
+    public string RadiusSubscriptionTenantId { get; set; } = string.Empty;
 
     [JsonPropertyName("PolicyID")]
     public string? PolicyID { get; set; }
@@ -23,32 +23,44 @@ public class AuthenticationEventModel
     public bool Successful { get; set; }
 
     [JsonPropertyName("Message")]
-    public string Message { get; set; } = string.Empty;
+    public string? Message { get; set; }
 
     [JsonPropertyName("UserName")]
-    public string UserName { get; set; } = string.Empty;
+    public string? UserName { get; set; }
 
     [JsonPropertyName("CertificateThumbprint")]
-    public string CertificateThumbprint { get; set; } = string.Empty;
+    public string? CertificateThumbprint { get; set; }
+
+    [JsonPropertyName("CertificateSubjectName")]
+    public string? CertificateSubjectName { get; set; }
 
     [JsonPropertyName("Certificate")]
-    public string Certificate { get; set; } = string.Empty;
+    public string? Certificate { get; set; }
 
     [JsonPropertyName("VLanName")]
-    public string VLanName { get; set; } = string.Empty;
+    public string? VLanName { get; set; }
 
     [JsonPropertyName("FilterID")]
-    public string FilterID { get; set; } = string.Empty;
+    public string? FilterID { get; set; }
 
     [JsonPropertyName("AuthenticationType")]
-    public string AuthenticationType { get; set; } = string.Empty;
+    public string? AuthenticationType { get; set; }
 
     [JsonPropertyName("DateCreated")]
     public DateTimeOffset DateCreated { get; set; } = DateTime.UtcNow;
 
     [JsonPropertyName("RequestingIP")]
-    public string RequestingIP { get; set; } = string.Empty;
+    public string? RequestingIP { get; set; }
 
     [JsonPropertyName("RADIUSIP")]
-    public string RADIUSIP { get; set; } = string.Empty;
+    public string? RADIUSIP { get; set; }
+
+    [JsonPropertyName("NasIpAddress")]
+    public string? NasIpAddress { get; set; }
+
+    [JsonPropertyName("NasIdentifier")]
+    public string? NasIdentifier { get; set; }
+
+    [JsonPropertyName("RadsecCertificate")]
+    public string? RadsecCertificate { get; set; }
 }
